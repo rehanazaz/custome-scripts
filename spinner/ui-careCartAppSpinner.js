fd98c52448e3e37c1036ab59dd97dc72b3598662
mr_farooq <muhammad.farooq@vaivaltech.com>
2021-11-08 14:26:54 +0500

    Custom page support added for store bighammerwines-com-2.myshopify.com

--- spinner/ui-careCartAppSpinner.js
+++ spinner/ui-careCartAppSpinner.js
@@ -1104,6 +1104,16 @@
 									return;
 								}
 							}
+							if (Shopify.shop == 'bighammerwines-com-2.myshopify.com') {
+								var thisStatus = checkStoreSpecificUrlCcSpinASale("https://www.bighammerwines.com/pages/blackfriday");
+								//console.log('checkStoreSpecificUrlCcSpinASale Status: ' + thisStatus);
+								if (thisStatus) {
+									console.log('SAS Custom Page Matched for store: ' + Shopify.shop);
+								} else {
+									console.log('SAS Custom Page NOT Matched for store: ' + Shopify.shop);
+									return;
+								}
+							}
 							/* New custom fixes added from here */
 							if (Shopify.shop == 'tweetprintshop.myshopify.com') {
 								carecartSpinnerJquery("body").append('<style type="text/css"> .wheelify-content-spinner {background-color: #008affd1 !important;} #wheelify-spin-trigger-cc img {opacity: 1}</style>');
